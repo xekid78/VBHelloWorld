f567aaa94a0c16433f38ddd361938e3a120f3061
Makoto Tanabe <xekid78@outlook.com>
2018-03-11 20:46:32 +0900

    Add files via upload

---
+++ Hello.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Hello
+    Sub Main(args As String())
+        Console.WriteLine("Hello World")
+
+        Console.Write("何かボタンを押したら終了します。")
+        Console.ReadKey(True)
+    End Sub
+End Module
